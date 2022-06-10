bf51be3ffba0ad0f3505a8efa19d2b972112dfb1
Eric Torstenson <eric.s.torstenson@vumc.org>
2022-06-10 10:03:56 -0500

    Added support for embedding one table as part of another

---
+++ wstlr/embedable.py
@@ -0,0 +1,37 @@
+"""
+ * Provide ability to embed rows from one table as members of a different table
+ * based on a specified common ID column
+"""
+
+import collections
+from csv import DictReader
+
+class EmbedableTable:
+    def __init__(self, table_name, target_table, join_column):
+        self.table_name = table_name
+        self.target = target_table
+        self.join_col = join_column
+
+        # There can be more than one matching row per ID 
+        self.rows = collections.defaultdict(list)
+        self.column_names = []
+
+    def load_data(self, filename):
+        with open(filename, 'rt') as f:
+            reader = DictReader(f, delimiter=',', quotechar='"')
+            self.column_names = reader.fieldnames
+
+            assert(self.join_col in self.column_names)
+
+            for line in reader:
+                id = line[self.join_col]
+                self.rows[id].append(line)
+
+    def get_rows(self, id):
+        rows = []
+        if id in self.rows:
+            for row in self.rows[id]:
+                rows.append({"table_name": self.table_name})
+                rows[-1].update(row)
+
+        return rows
