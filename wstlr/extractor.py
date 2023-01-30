286e345ec4b45d83e7ff5bc32a33f7b99fcf154a
Eric Torstenson <eric.s.torstenson@vumc.org>
2023-01-30 10:55:55 -0600

    Pass one of using config object in place of dictionary

--- wstlr/extractor.py
+++ wstlr/extractor.py
@@ -12,9 +12,11 @@ from copy import deepcopy
 import pdb 
 from wstlr.conceptmap import ObjectifyHarmony
 from wstlr.embedable import EmbedableTable
-from wstlr import dd_system_url, system_base, StandardizeDdType, clean_values, fix_fieldname
+from wstlr import dd_system_url, StandardizeDdType, clean_values, fix_fieldname
 
 from wstlr import system_base, InvalidType
+
+from wstlr.config import Configuration
 
 default_colnames = {
     "varname": "varname",
@@ -98,8 +100,6 @@ class GroupBy:
         for col in row.keys():
             if col not in self.group_by:
                 current[col] = row[col]
-
-
 
 class DataDictionaryVariableCS:
     def __init__(self, study, consent_group, table_name, varname, values, url_base=system_base):
@@ -362,16 +362,16 @@ def DataCsvToObject(config):
 
     dataset = {
         "study": {
-            "id": config['study_id'],
-            "accession": config['study_accession'],
-            "title": config['study_title'],
-            "desc": config['study_desc'],
-            "identifier-prefix": config['identifier_prefix'],
-            "url": config['url'],
-            "publisher": config.get('publisher'),
+            "id": config.study_id,
+            "accession": config.study_accession,
+            "title": config.study_title,
+            "desc": config.study_desc,
+            "identifier-prefix": config.identifier_prefix,
+            "url": config.url,
+            "publisher": config.publisher,
             "data-dictionary": [
                 {
-                    "study": config['study_id'],
+                    "study": config.study_id,
                     "values": []
                 }
             ]
@@ -380,18 +380,18 @@ def DataCsvToObject(config):
         "harmony": [],
     }
 
-    if 'remote_data_access' in config:
+    if config.remote_data_access is not None:
         dataset['study']['remote_access'] = {
-            "type": config['remote_data_access']['access_type'],
-            "url": config['remote_data_access']['access_url']
+            "type": config.remote_data_access['access_type'],
+            "url": config.remote_data_access['access_url']
         }
 
-    if 'study_sponsor' in config:
-        dataset['study']['sponsor'] = config['study_sponsor']
+    if config.study_sponsor is not None:
+        dataset['study']['sponsor'] = config.study_sponsor
 
-    if 'consent_group' in config:
-        dataset['study']['consent_group'] = config['consent_group']
-        dataset['study']['consent_code'] = config['consent_group']['code']
+    if config.consent_group is not None:
+        dataset['study']['consent_group'] = config.consent_group
+        dataset['study']['consent_code'] = config.consent_group['code']
     
     if dataset['study']['publisher'] is None:
         dataset['study']['publisher'] = "NCPI FHIR Working Group"
@@ -401,10 +401,16 @@ def DataCsvToObject(config):
     if consent_group is not None:
         study_component = f"{study_component}-{consent_group}"
 
-    dd_tablevar_cs = DataDictionaryVariableCS(config['study_id'], consent_group, "DataSet", None, "", url_base=config['identifier_prefix'])
+    dd_tablevar_cs = DataDictionaryVariableCS(config.study_id, 
+                                    consent_group, 
+                                    "DataSet", 
+                                    None, 
+                                    "", 
+                                    url_base=config.identifier_prefix)
+
     dataset['study']['data-dictionary'][0] = dd_tablevar_cs.as_obj()
 
-    active_tables = config.get('active_tables')
+    active_tables = config.active_tables
     if active_tables is None:
         active_tables['ALL'] = True
 
@@ -413,10 +419,10 @@ def DataCsvToObject(config):
 
     embedded = defaultdict(list)
 
-    for category in config['dataset'].keys():
-        embedable = config['dataset'][category].get('embed')
+    for category,table in config.dataset.items():
+        embedable = table.get('embed')
 
-        filenames = config['dataset'][category]['filename'].split(",")
+        filenames = table['filename'].split(",")
 
         dd_tablevar_cs.values[category] = ",".join([fn.split("/")[-1] for fn in filenames])
 
@@ -428,21 +434,21 @@ def DataCsvToObject(config):
 
         if embedable is not None:
             embd = EmbedableTable(category, embedable['dataset'], embedable['colname'])
-            for filename in config['dataset'][category]['filename'].split(","):
+            for filename in table['filename'].split(","):
                 embd.load_data(filename)
             embedded[embd.target].append(embd)
 
-    for category in config['dataset'].keys():
+    for category, table in config.dataset.items():
         data_chunk = []
         aggregators = {}
         agg_splitter = None
-        if 'aggregators' in config['dataset'][category]:
-            agg_splitter = config['dataset'][category].get('aggregator-splitter')
-            aggregators = BuildAggregators(config['dataset'][category]['aggregators'])
+        if 'aggregators' in table:
+            agg_splitter = table.get('aggregator-splitter')
+            aggregators = BuildAggregators(table['aggregators'])
         
         code_details = {}
-        if 'code_harmonization' in config['dataset'][category]:
-            with open(config['dataset'][category]['code_harmonization'], 'rt') as f:
+        if 'code_harmonization' in table:
+            with open(table['code_harmonization'], 'rt') as f:
                 reader = csv.DictReader(f, delimiter=',', quotechar='"')
 
                 for row in reader:
@@ -451,9 +457,13 @@ def DataCsvToObject(config):
                     code_details[row['local code']] = row['display']
         dataset['study']['data-dictionary'][0]['values'].append({
             "varname": category,
-            "desc": ",".join([x.split("/")[-1] for x in config['dataset'][category]['filename'].split(",")]),
+            "desc": ",".join([x.split("/")[-1] for x in table['filename'].split(",")]),
             "type": "DD-Table",
-            "url": dd_system_url(config['identifier_prefix'], "CodeSystem", study_component, category, varname=None),
+            "url": dd_system_url(config.identifier_prefix, 
+                                "CodeSystem", 
+                                study_component, 
+                                category, 
+                                varname=None),
             "values": []
         })
 
@@ -463,22 +473,22 @@ def DataCsvToObject(config):
         # to identify those artificial questions and assign those to the final output instead
         # of the long, descriptive name
         dd_based_varnames = {}
-        if 'data_dictionary' in config['dataset'][category]:
+        if 'data_dictionary' in table:
             # print(config['dataset'][category]['data_dictionary']['filename'])
-            with open(config['dataset'][category]['data_dictionary']['filename'], 'rt', encoding='utf-8-sig') as f:
+            with open(table['data_dictionary']['filename'], 'rt', encoding='utf-8-sig') as f:
                 delimiter = ","
-                if 'delimiter' in config['dataset'][category]['data_dictionary']:
-                    delimiter = config['dataset'][category]['data_dictionary']['delimiter']
+                if 'delimiter' in table['data_dictionary']:
+                    delimiter = table['data_dictionary']['delimiter']
 
-                dd, cs_values = ObjectifyDD(config['study_id'], 
+                dd, cs_values = ObjectifyDD(config.study_id, 
                                                 consent_group, 
                                                 category, 
                                                 f, 
                                                 dd_codesystems, 
-                                                config['dataset'][category]['data_dictionary'].get('colnames'), 
+                                                table['data_dictionary'].get('colnames'), 
                                                 delimiter=delimiter, 
-                                                subject_id=config['dataset'][category].get("subject_id"),
-                                                url_base=config['identifier_prefix'])
+                                                subject_id=table.get("subject_id"),
+                                                url_base=config.identifier_prefix)
                 
                 # fill out the 'values' list for each of the vars
                 for cs_entry in cs_values:
@@ -494,25 +504,33 @@ def DataCsvToObject(config):
                 dd_based_varnames = build_varname_lookup(dd)
 
         if active_tables.get('ALL') == True or active_tables.get("harmony"):
-            if 'code_harmonization' in config['dataset'][category]:
-                harmony_file = config['dataset'][category]['code_harmonization']
+            if 'code_harmonization' in table:
+                harmony_file = table['code_harmonization']
                 if harmony_file not in harmony_files:
                     harmony_files.add(harmony_file)
-                    dataset['harmony'].append(ObjectifyHarmony(harmony_file, curies=config.get('curies'), study_component=study_component))
+                    dataset['harmony'].append(ObjectifyHarmony(harmony_file, 
+                                                            curies=config.curies, 
+                                                            study_component=study_component))
 
         if active_tables.get('ALL') == True or active_tables.get(category):
-            if 'embed' not in config['dataset'][category]:
-                grouper = GroupBy(config=config['dataset'][category].get('group_by'))
-                file_list = [x.strip() for x in config['dataset'][category]['filename'].split(",")]
+            if 'embed' not in table:
+                grouper = GroupBy(config=table.get('group_by'))
+                file_list = [x.strip() for x in table['filename'].split(",")]
 
                 for filename in file_list:
                     if filename.lower() != 'none':
                         with open(filename, encoding='utf-8-sig', errors='ignore') as f:
                             delimiter = ","
-                            if 'delimiter' in config['dataset'][category]:
-                                delimiter = config['dataset'][category]['delimiter']
+                            if 'delimiter' in table:
+                                delimiter = table['delimiter']
 
-                            data_chunk = ObjectifyCSV(f, aggregators, grouper, agg_splitter, code_details, dd_based_varnames, delimiter=delimiter)
+                            data_chunk = ObjectifyCSV(f, 
+                                                    aggregators, 
+                                                    grouper, 
+                                                    agg_splitter, 
+                                                    code_details, 
+                                                    dd_based_varnames, 
+                                                    delimiter=delimiter)
 
                             if category in embedded:
                                 for emb in embedded[category]:
@@ -571,12 +589,13 @@ def exec(args=None):
                         default='output/whistle-input')
 
     args = parser.parse_args(args=args)
-    config = safe_load(args.config[0])
+
+    config = Configuration(args.config[0])
 
     # Work out the destination for the Whistle input
     output_directory = Path(args.output)
     output_directory.mkdir(parents=True, exist_ok=True)
-    output_filename = output_directory / f"{config['output_filename']}.json"
+    output_filename = output_directory / f"{config.output_filename}.json"
 
     dataset = DataCsvToObject(config, args.output_root, count=args.count)
     with output_filename.open(mode='wt') as f:
